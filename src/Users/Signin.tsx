7696ee361d40101d363adb0bfa522cf13e7e927d
stevennguyen003 <96093087+stevennguyen003@users.noreply.github.com>
2024-04-14 21:28:03 -0400

    style

--- src/Users/Signin.tsx
+++ src/Users/Signin.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { User } from "./client";
 import * as client from "./client";
+import "./index.css";
 export default function Signin() {
   const [credentials, setCredentials] = useState<User>({
     _id: "",
@@ -19,12 +20,14 @@ export default function Signin() {
     <>
       <div className="account-signin-container">
         <h1>Signin</h1>
+        <h3>Username</h3>
         <input value={credentials.username} onChange={(e) =>
           setCredentials({ ...credentials, username: e.target.value })} /> <br />
+        <h3>Password</h3>
         <input value={credentials.password} onChange={(e) =>
           setCredentials({ ...credentials, password: e.target.value })} /> <br />
-        <button className="btn btn-primary" onClick={signin}> Signin </button> <br/>
-        <button className="btn btn-primary" onClick={signup}> Signup </button>
+        <button className="btn btn-primary" onClick={signin}> Signin </button> <br />
+        <button className="btn btn-warning" onClick={signup}> Signup </button>
       </div>
     </>
   );
--- src/Users/Signup.tsx
+++ src/Users/Signup.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import "./index.css";
 import * as client from "./client";
 export default function Signup() {
   const [error, setError] = useState("");
@@ -14,16 +15,18 @@ export default function Signup() {
     }
   };
   return (
-    <div>
+    <div className="account-container">
       <h1>Signup</h1>
       {error && <div>{error}</div>}
+      <h3>Username</h3>
       <input value={user.username} onChange={(e) => setUser({
         ...user, username: e.target.value
       })} /> <br />
+      <h3>Password</h3>
       <input value={user.password} onChange={(e) => setUser({
         ...user, password: e.target.value
       })} /> <br />
-      <button className="btn btn-primary" onClick={signup}> Signup </button> <br />
+      <button className="btn btn-warning" onClick={signup}> Signup </button> <br />
     </div>
   );
 }
